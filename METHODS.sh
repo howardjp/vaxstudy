78ef94cb5af9b7b5c12f7ded1d149707ec1d57d1
James Howard <james.howard@jhu.edu>
2021-07-27 16:54:54 -0400

    Run all the sims

--- METHODS.sh
+++ METHODS.sh
@@ -8,10 +8,6 @@ list_of_vax_uptake="0.25 0.30 0.35 0.40 0.45 0.50 0.55 0.60 0.65 0.70 0.75"
 list_of_min_ages="0 12 18"
 list_of_vaxavailperday="1000 5000 10000 25000 50000"
 
-list_of_trans="1.0"
-list_of_vax_uptake="0.50"
-list_of_min_ages="12"
-
 for i in $list_of_vax_uptake; do
     for j in $list_of_min_ages; do
         echo "WILLINGNESS=$i MIN_AGE=$j m4 config.m4 > config.fred"
